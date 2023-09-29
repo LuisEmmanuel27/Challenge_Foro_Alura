06b258cb6245c82ef0b6f2d73613a88d28e9bcd1
Luis Emmanuel <96873849+LuisEmmanuel27@users.noreply.github.com>
2023-09-29 10:09:16 -0600

    Agregado un tratador de errores global Se agrego un pequeño tratador de errores, que de momento maneja los errores 400 y 404

---
+++ api/src/main/java/foro/alura/luis/api/infra/TratadorDeErrores.java
@@ -0,0 +1,34 @@
+package foro.alura.luis.api.infra;
+
+import java.util.List;
+
+import jakarta.persistence.EntityNotFoundException;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+
+@RestControllerAdvice
+public class TratadorDeErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Void> tratarError404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DatosErrorValidacion>> tratarError400(MethodArgumentNotValidException e) {
+        var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    private record DatosErrorValidacion(String campo, String error) {
+        public DatosErrorValidacion(FieldError error) {
+            this(
+                    error.getField(),
+                    error.getDefaultMessage());
+        }
+    }
+}
