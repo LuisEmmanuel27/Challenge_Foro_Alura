52af17da716ad7148f118f4cb9a02a20d5fe0db6
Luis Emmanuel <96873849+LuisEmmanuel27@users.noreply.github.com>
2023-10-17 12:11:07 -0600

    Cambios menores

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ import Foro from "./pages/Foro";
 import CrearTopic from "./pages/CrearTopic";
 import VerTopico from "./pages/VerTopico";
 import RutasPrivadas from "./components/RutasPrivadas";
-import Temp from "./pages/Temp";
 import EditarTopico from "./pages/EditarTopico";
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
         <Route path="/" element={<Inicio />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sign_up" element={<SignUp />} />
-        <Route path="/temp" element={<Temp />} />
         <Route path="/foro" element={<RutasPrivadas><Foro /></RutasPrivadas>} />
         <Route path="/foro/crear" element={<RutasPrivadas><CrearTopic /></RutasPrivadas>} />
         <Route path="/foro/topico/:auth/:id" element={<RutasPrivadas><VerTopico /></RutasPrivadas>} />
